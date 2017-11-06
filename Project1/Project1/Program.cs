da7c03ca40f550c6cb887ce976ee7f58a3dd5abb
zanelias <shafaat.ahsen@gmail.com>
2017-11-05 22:23:06 -0600

    Added a loop statement

--- Project1/Project1/Program.cs
+++ Project1/Project1/Program.cs
@@ -2,16 +2,16 @@
 
 namespace Project1
 {
-    class Program
+    class Program // class contains program and also matches with name
     {
-        static void Main()
+        static void Main()  //Method Main has to be in every program, is where program starts running
         {
             double width, height, woodLength, glassArea;
             string widthString, heightString;
             widthString = Console.ReadLine();  //reads user input
             width = double.Parse(widthString);
             heightString = Console.ReadLine();
-            height = double.Parse(heightString);
+            height = double.Parse(heightString);    
             woodLength = 2 * (width + height ) * 3.25 ;
             glassArea = 2 * (width* height ) ;
             Console.WriteLine( "The length of the wood is " +
@@ -22,6 +22,7 @@ namespace Project1
             Console.ReadKey();
 
         }
-        
+        
+
     }
 }
