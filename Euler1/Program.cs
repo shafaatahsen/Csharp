0f8611a3458acfe467c8fcbf16e2c8bf076e0b14
zanelias <shafaat.ahsen@gmail.com>
2017-11-20 21:49:01 -0600

    added Euler Project

--- Euler1/Program.cs
+++ Euler1/Program.cs
@@ -6,7 +6,31 @@ namespace Euler1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Optimized method with continuous sum function
+            int sum = 0;
+            int a = 3;
+            int b = 5;
+            int range = 999; //range has to be either range or raneg -1 so that value isn't a multiple
+            int aprime = ContinuousSum(range/a);
+            int bprime = ContinuousSum(range/b); 
+            int common = ContinuousSum(range/(a*b));
+            int sum1 = a*aprime + b*bprime - a*b*common;
+            Console.WriteLine(sum1);
+
+            // a Super slow iterative method
+            for (int i = 3 ; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum = i + sum;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+        static public int ContinuousSum(int x)
+        {
+            int result = x*(x+1)/2;
+            return result;
         }
     }
 }
